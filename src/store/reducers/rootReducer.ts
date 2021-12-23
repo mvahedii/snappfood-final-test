cb37670cb9c2af3b54e2848dafc816b557351b03
mvahedi <vahedi.ce@gmail.com>
2021-12-23 22:53:18 +0330

    feat: add redux & gate

--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -1,16 +1,14 @@
 import {combineReducers, Reducer} from 'redux';
 import vendorsReducer from './vendorsReducer';
 
-export interface RootState {
-  vendorsStore: ReturnType<typeof vendorsReducer>;
-}
-
 export const rootReducer: Reducer<RootState, any> = (root, action) => {
   return {
     vendorsStore: vendorsReducer(root?.vendorsStore, action),
   };
 };
 
-export type AppState = ReturnType<typeof rootReducer>;
+export interface RootState {
+  vendorsStore: ReturnType<typeof vendorsReducer>;
+}
 
 export default rootReducer;
